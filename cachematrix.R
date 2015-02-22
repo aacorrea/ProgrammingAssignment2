6cce174cfcf0b4b5441cdedbfd5c42e602ba6823
Alejandro <alejandromagallanes@gmail.com>
2015-02-21 21:26:40 -0430

    <updated>

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,54 @@
-## Put comments here that give an overall description of what your
-## functions do
+## For a large matrix, it may take too long to compute its inverse, 
+##especially if it has to be computed repeatedly. 
+##If the contents of the matrix are not changing, it may make sense 
+##to cache the value of its inverse so that when we need it again, 
+##it can be looked up in the cache rather than recomputed
 
-## Write a short comment describing this function
+## 1.-This function creates a special "matrix" object that can cache 
+##its inverse.
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(M = matrix()) {
+    
+    inv_M <- NULL
+    
+    set <- function(A) {    ## Set the value of the matrix
+        M <<- A
+        inv_M <<- NULL
+    }
+    
+    get <- function() M      ## Get the value of the matrix
+    
+    setinverse <- function(Minverse) inv_M <<- Minverse     ## Set the value of the inverse matrix
+    
+    getinverse <- function() inv_M       ## Get the value of the inverse matrix
+    
+    list (set = set, get = get, 
+          setinverse = setinverse, 
+          getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## 2.-The next function computes the inverse of the special "matrix" returned 
+## by makeCacheMatrix above. If the inverse has already been calculated 
+## (and the matrix has not changed), then the cachesolve should retrieve 
+## the inverse from the cache
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(M = matrix(), ...) {
+    
+    inv_M <- M$getinverse()  
+    
+    if(!is.null(inv_M) && solve(inv_M) == M) {    ## Check if the inverse has been calculated   
+        message("getting cached data")            ## and if the matrix has not changed 
+        return(inv_M) }
+    
+    
+    data <- M$get()             ## If the inverse doesn't exist or the matrix is not the same 
+    ## then we compute the inverse
+    inv_M <- solve(data, ...) 
+    
+    M$setinverse(inv_M)
+    
+    inv_M
 }
+
+## Execute in R for a given square matrix: cacheSolve(makeCacheMatrix())
